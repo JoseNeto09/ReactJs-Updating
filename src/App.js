f5f0b9113222400b709e8623af0793f0a6bf24ec
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-09-21 11:26:28 -0300

    tarefas com useState

--- src/App.js
+++ src/App.js
@@ -1,57 +1,38 @@
 import { useState } from 'react';
 
 function App() {
-  const [nome, setNome] = useState('');
-  const [idade, setIdade] = useState('');
-  const [Email, setEmail] = useState('');
-  
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'pagar conta de luz'
+  ]);
 
-  const [user, setUser] = useState({});
-
-
-  function handleRegister(e){
-   e.preventDefault();
-   alert('Register Success');
-    setUser({
-      nome: nome,
-      idade: idade,
-      Email: Email,
-    })
+function handleRegister(e){
+    e.preventDefault();
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
-  
+
   return (
     <div>
       <h1> Cadastrando Usuario </h1>
+
       <form onSubmit={handleRegister}>
         
-        <label>Nome: </label><br/>
+        <label>Nome da tarefa:</label><br/>
         <input 
-          placeholder='Digute seu nome' 
-          value={nome}
-          onChange={ (e) => setNome(e.target.value) }
+          placeholder='Digute uma tarefa' 
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
           /><br/>
-
-        <label>Idade: </label><br/>
-        <input placeholder='Digute sua idade' 
-          value={idade}
-          onChange={ (e) => setIdade(e.target.value) }
-          /><br/>
-
-        <label>Email: </label><br/>
-        <input placeholder='Digute seu email'  
-          value={Email}
-          onChange={ (e) => setEmail(e.target.value) }
-          /><br/>
-
         <button type='submit'>Registrar</button>
       </form>
+  
+      <br/><br/>
+      
       <div>
-        <br/><br/>
-      <div>
-        <span>Bem vindo: {user.nome}</span><br/>
-        <span>Idade: {user.idade}</span><br/>
-        <span>Email: {user.Email}</span><br/>
-      </div>
+        {tarefas.map( tarefas => (
+          <li key={tarefas}>{tarefas}</li>
+        ))}
       </div>
     </div>
   );
